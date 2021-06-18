a8f3498ea5a0de08f0198424ef1d06a49534ef94
Borovkov Sergey <707grays@gmail.com>
2021-06-18 23:28:25 +0700

    added empty project

---
+++ PetFrameworksLearning/PetFrameworksLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFrameworksLearningApp.swift
+//  PetFrameworksLearning
+//
+//  Created by Sergey Borovkov on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PetFrameworksLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
